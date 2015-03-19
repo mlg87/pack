08241eda606cf96a48b95f4eebb708d136dda9df
schuchard <kbs177@gmail.com>
2015-03-18 18:07:13 -0600

    add animation to search results

--- src/controllers/find.js
+++ src/controllers/find.js
@@ -4,27 +4,6 @@ var Activity = require('../models/activity.js');
 
 var findController = {
 
-  // initialize: function(req, res){
-  //   res.render('find', {
-  //     users: user.users,
-  //     globalEvents : user.globalEvents,
-  //     push : user.push
-  //   });
-  // },
-
-  // searchForm: function(req, res){
-  //   var matches = [];
-  //   matches = user.matchActivity(
-  //     req.body.type,
-  //     req.body.date,
-  //     req.body.time,
-  //     req.body.distance,
-  //     req.body.pace,
-  //     req.body.address
-  //   );
-  //   // deleteMarkers();
-  //   // user.displayMatches(matches)
-  // },
 
   getAll: function(req, res){
     if(req.query._id){
