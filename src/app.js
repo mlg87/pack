3cd3e56239de765898ca5ad7697961ea388b930a
schuchard <kbs177@gmail.com>
2015-04-13 14:44:29 -0600

    add jwt dependencies

--- src/app.js
+++ src/app.js
@@ -19,6 +19,11 @@ var passport = require('passport');
 // See users to DB
 var passportConfig = require('./config/passport');
 
+// +++++++++++ Token auth
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+
+
 // Seed the DB with activities
 require('./models/seeds/activitySeeds.js');
 
@@ -67,7 +72,45 @@ app.use(passport.initialize());
 // Hook in the passport session management into the middleware chain.
 app.use(passport.session());
 
+// +++++++++++ Protect /api routes with JWT
+app.use('/api', expressJwt({secret: 'secret'}));
+app.use(bodyParser.json());
+
 app.get('/', indexController.index);
+
+
+// +++++++++++
+app.post('/authenticate', function (req, res) {
+  //TODO validate req.body.username and req.body.password
+  //if is invalid, return 401
+  if (!(req.body.username === 'john.doe' && req.body.password === 'foobar')) {
+    res.send(401, 'Wrong user or password');
+    return;
+  }
+
+  var profile = {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'john@doe.com',
+    id: 123
+  };
+
+  // We are sending the profile inside the token
+  var token = jwt.sign(profile, secret, { expiresInMinutes: 60*5 });
+
+  res.json({ token: token });
+});
+
+app.get('/api/restricted', function (req, res) {
+  console.log('user ' + req.user.email + ' is calling /api/restricted');
+  res.json({
+    name: 'foo'
+  });
+});
+
+
+
+
 
 app.get('/users',
   passportConfig.ensureAuthenticated,
